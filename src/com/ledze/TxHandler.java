083c29923ca96476573959decc44c4a6dda3238c
gpuchetaCC <Gerardo.Pucheta@compucom.com>
2018-01-24 17:49:27 -0600

    Corrección en la validación de una transacción e implementación de la llegada de un nuevo bloque.

--- src/com/ledze/TxHandler.java
+++ src/com/ledze/TxHandler.java
@@ -18,62 +18,68 @@ public class TxHandler {
     }
 
     /**
-     * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
-     * (3) no UTXO is claimed multiple times by {@code tx},
-     * (4) all of {@code tx}s output values are non-negative, and
+     * @return true if: </br>
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, </br>
+     * (2) the signatures on each input of {@code tx} are valid, </br>
+     * (3) no UTXO is claimed multiple times by {@code tx}, </br>
+     * (4) all of {@code tx}s output values are non-negative, and </br>
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
+     * values; and false otherwise. </br>
      */
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
         // 1.
-        boolean valid;
-        valid = pool.getAllUTXO().containsAll(tx.getOutputs());
-        if ( !valid ) return false;
+        boolean valid = true;
+        UTXO _utxo = null;
+        for(Transaction.Input input : tx.getInputs() ){
+            _utxo = new UTXO(tx.getHash(), input.outputIndex);
+            valid = pool.getAllUTXO().contains(_utxo);
+            if( !valid )break;
+        }
+        if( !valid ) return false;
 
         // 2.
         valid = tx.getInputs()
                 .parallelStream()
-                .anyMatch(input -> !Crypto.verifySignature( tx.getOutput(input.outputIndex).address,
-                                                            tx.getRawDataToSign(input.outputIndex),
-                                                            input.signature) );
-        if( valid ) return false;
+                .anyMatch(input -> !Crypto.verifySignature(tx.getOutput(input.outputIndex).address,
+                        tx.getRawDataToSign(input.outputIndex),
+                        input.signature));
+        if (valid) return false;
 
         //3.
-        for(UTXO utxo : pool.getAllUTXO()){
+        for (UTXO utxo : pool.getAllUTXO()) {
             long howmany = tx.getOutputs()
                     .stream()
                     .filter(x -> x.equals(pool.getTxOutput(utxo)))
                     .count();
             System.out.println("howmany:" + howmany);
-            if(howmany > 1) {
+            if (howmany > 1) {
                 valid = false;
-            }else {
+            } else {
                 valid = true;
             }
         }
-        if ( !valid ) return false;
+        if (!valid) return false;
 
         //4.
-        for(Transaction.Output o : tx.getOutputs()){
-            if ( isNegative(o.value) ) {
+        for (Transaction.Output o : tx.getOutputs()) {
+            if (isNegative(o.value)) {
                 valid = false;
                 break;
-            };
+            }
+            ;
         }
-        if ( !valid ) return false;
+        if (!valid) return false;
 
         //5.
         double inputValSum = tx.getInputs()
                 .stream()
-                .flatMapToDouble(i -> DoubleStream.of(tx.getOutput(i.outputIndex).value) )
+                .flatMapToDouble(i -> DoubleStream.of(tx.getOutput(i.outputIndex).value))
                 .sum();
 
         double outputValSum = tx.getOutputs()
                 .stream()
-                .flatMapToDouble(o -> DoubleStream.of(o.value) )
+                .flatMapToDouble(o -> DoubleStream.of(o.value))
                 .sum();
 
         return inputValSum >= outputValSum;
@@ -90,17 +96,70 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        Transaction[] t = null;
         // IMPLEMENT THIS
         ArrayList<Transaction> l = new ArrayList<>();
-        for ( int i = 0 ; i<possibleTxs.length ; i++){
-            if(isValidTx(possibleTxs[i])){
+        ArrayList<Transaction.Output> lstOutputs = new ArrayList<>();
+        for (int i = 0; i < possibleTxs.length; i++) {
+            boolean doubleSpending = false;
+            if (isValidTx(possibleTxs[i])) {
+
+                if (lstOutputs.isEmpty()) {
+                    ArrayList<Transaction.Output> lstFirstOutputs = new ArrayList<>();
+                    for( Transaction.Output o : possibleTxs[i].getOutputs() ){
+                        if(lstFirstOutputs.isEmpty()){
+                            lstFirstOutputs.add(o);
+                        }else{
+                            if( !lstFirstOutputs.contains(o) ){
+                                lstFirstOutputs.add(o);
+                            }
+                        }
+                    }
+                    lstOutputs.addAll(lstFirstOutputs);
+                } else {
+                    for (Transaction.Output output : possibleTxs[i].getOutputs()) {
+                        doubleSpending = lstOutputs.contains(output);
+                        if (doubleSpending) {
+                            break;
+                        }
+                    }
+                    if (doubleSpending) {
+                        System.out.println("WARNING!!!: Double spending, the Tx will not be added to the ledger.");
+                        continue;
+                    }
+                }
                 l.add(possibleTxs[i]);
             }
         }
 
+        for(Transaction tx: l){
+            byte[] txHash = tx.getHash();
 
-        return t;
+            for(Transaction.Input input : tx.getInputs()){
+                pool.addUTXO(
+                        new UTXO(txHash, input.outputIndex),
+                        tx.getOutput(input.outputIndex)
+                );
+            }
+        }
+
+        return l.stream().toArray(Transaction[]::new);
     }
+
+   /* public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            boolean doubleSpending = false;
+            System.out.println("i: "+i);
+            for (int j = 0; j < 10; j++) {
+                System.out.println("j: "+j);
+                doubleSpending = (j==8);
+                if(doubleSpending)break;
+            }
+            if(doubleSpending){
+                System.out.println("double spending");
+                break;
+            }
+        }
+
+    }*/
 
 }
